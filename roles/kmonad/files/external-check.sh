acb49e4efec52368911ce681d3b8683ec3296f71
Cam Stark <cam@cystark.com.au>
2023-10-04 07:07:42 +1000

    feat(kmonad): external-check script

--- roles/kmonad/files/external-check.sh
+++ roles/kmonad/files/external-check.sh
@@ -4,4 +4,6 @@ device_name="usb-Keychron_Keychron_K2-event-kbd"
 
 if [[ -e "/dev/input/by-id/${device_name}" ]]; then
     kmonad /home/camstark/.config/kmonad/keyboard.kbd
+else
+    kmonad /home/camstark/.config/kmonad/laptop.kbd
 fi
