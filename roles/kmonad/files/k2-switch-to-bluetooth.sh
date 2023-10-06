624afa745e5a36328328f7378d20005d2dbdd952
Cam Stark <cam@cystark.com.au>
2023-10-07 09:49:46 +1100

    Update kmonad service to use the new k2-switch-to-bluetooth script

--- roles/kmonad/files/k2-switch-to-bluetooth.sh
+++ roles/kmonad/files/k2-switch-to-bluetooth.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 # Based off: https://github.com/MaxGyver83/dotfiles/blob/972d7d2379e4eacfdabadd5d895500a4c1dbce0f/scripts/bin/start-kmonad-for-all-keyboards.fish
 
-KB_SEARCH="bluetooth"
-BLUETOOTH_KB_EVENT_ID="$(grep -B 9 -A 4 12001 /proc/bus/input/devices | grep "${KB_SEARCH}" -A 4 | grep -oE 'event[0-9]+')"
-EVENT_PATH="/dev/input/${BLUETOOTH_KB_EVENT_ID}"
-PATH_TO_FILE="${HOME}/.config/kmonad/keyboard.kbd"
-KEYCHRON_KBD="/dev/input/by-id/usb-Keychron_Keychron_K2-event-kbd"
+HOME="/home/camstark"
 
-FLAG="${1}";
+BT_KB_SEARCH="Keychron K2"
+BT_KB_EVENT_ID="$(grep -B 9 -A 4 12001 /proc/bus/input/devices | grep "${BT_KB_SEARCH}" -A 4 | grep -oE 'event[0-9]+')"
+BT_EVENT_PATH="/dev/input/${BT_KB_EVENT_ID}"
 
-if [ -z "${FLAG}" ]; then
-  FLAG="--bluetooth"
+KB_KMONAD_PATH="${HOME}/.config/kmonad/keyboard.kbd"
+KEYCHRON_KBD_ID="/dev/input/by-id/usb-Keychron_Keychron_K2-event-kbd"
+
+LATOP_KMONAD_PATH="${HOME}/.config/kmonad/laptop.kbd"
+TEMP_CURRENT="/tmp/kmonad-connection"
+
+if [[ -e "${BT_EVENT_PATH}" ]]; then
+  # Switch out event id
+  sed -r -i 's%/dev/input/event[[:digit:]]+%'"${BT_EVENT_PATH}"'%g' "${KB_KMONAD_PATH}"
+  sed -i 's:'"${KEYCHRON_KBD_ID}"':'"${BT_EVENT_PATH}"':g' "${KB_KMONAD_PATH}"
+
+  sudo kmonad "${KB_KMONAD_PATH}"
+
+elif [[ -e "${KEYCHRON_KBD_ID}" ]]; then
+  # Switch out event id
+  #
+  sed -r -i 's%/dev/input/event[[:digit:]]+%'"${KEYCHRON_KBD_ID}"'%g' "${KB_KMONAD_PATH}"
+
+  sudo kmonad "${KB_KMONAD_PATH}"
+else 
+
+  sudo kmonad "${LATOP_KMONAD_PATH}"
 fi
-
-if [ "${FLAG}" == "--bluetooth" ]; then
-  sed -r -i 's%/dev/input/event[[:digit:]]+%'"${EVENT_PATH}"'%g' "${PATH_TO_FILE}"
-  sed -i 's:'"${KEYCHRON_KBD}"':'"${EVENT_PATH}"':g' "${PATH_TO_FILE}"
-elif [ "${FLAG}" == "--cable" ]; then
-  sed -r -i 's%/dev/input/event[[:digit:]]+%'"${KEYCHRON_KBD}"'%g' "${PATH_TO_FILE}"
-fi
-
-# Restart the kmonad service to init the new bluetooth path
-sudo systemctl restart kmonad
