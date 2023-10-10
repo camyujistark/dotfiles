7403b8f4563e67085c1f615ff4630cd93f82b58d
Cam Stark <cam@cystark.com.au>
2023-10-10 17:38:35 +1100

    feat: Kmonad updaets

--- roles/kmonad/files/k2-switch-to-bluetooth.sh
+++ roles/kmonad/files/k2-switch-to-bluetooth.sh
@@ -5,25 +5,29 @@ HOME="/home/camstark"
 
 BT_KB_SEARCH="Keychron K2"
 BT_KB_EVENT_ID="$(grep -B 9 -A 4 12001 /proc/bus/input/devices | grep "${BT_KB_SEARCH}" -A 4 | grep -oE 'event[0-9]+')"
-BT_EVENT_PATH="/dev/input/${BT_KB_EVENT_ID}"
+BT_INPUT_PATH="/dev/input/${BT_KB_EVENT_ID}"
 
 KB_KMONAD_PATH="${HOME}/.config/kmonad/keyboard.kbd"
-KEYCHRON_KBD_ID="/dev/input/by-id/usb-Keychron_Keychron_K2-event-kbd"
+KEYCHRON_INPUT_PATH="/dev/input/by-id/usb-Keychron_Keychron_K2-event-kbd"
 
 LATOP_KMONAD_PATH="${HOME}/.config/kmonad/laptop.kbd"
-TEMP_CURRENT="/tmp/kmonad-connection"
 
-if [[ -e "${BT_EVENT_PATH}" ]]; then
-  # Switch out event id
-  sed -r -i 's%/dev/input/event[[:digit:]]+%'"${BT_EVENT_PATH}"'%g' "${KB_KMONAD_PATH}"
-  sed -i 's:'"${KEYCHRON_KBD_ID}"':'"${BT_EVENT_PATH}"':g' "${KB_KMONAD_PATH}"
+if [[ "${BT_KB_EVENT_ID}" ]]; then
+  # If eventID exists then switch to new bt input path
+  sed -r -i 's%"/dev/input/event[[:digit:]]+"%'"\"${BT_INPUT_PATH}\""'%g' "${KB_KMONAD_PATH}"
+
+  # If /dev/input/ exists then update to new bt input path
+  sed -i 's%"/dev/input/"%'"\"${BT_INPUT_PATH}\""'%g' "${KB_KMONAD_PATH}"
 
   sudo kmonad "${KB_KMONAD_PATH}"
 
-elif [[ -e "${KEYCHRON_KBD_ID}" ]]; then
-  # Switch out event id
-  #
-  sed -r -i 's%/dev/input/event[[:digit:]]+%'"${KEYCHRON_KBD_ID}"'%g' "${KB_KMONAD_PATH}"
+elif [[ -e "${KEYCHRON_INPUT_PATH}" ]]; then
+
+  # If bt input exists then switch to new bt input path
+  sed -i 's:'"${BT_INPUT_PATH}"':'"${KEYCHRON_INPUT_PATH}"':g' "${KB_KMONAD_PATH}"
+
+  # If /dev/input/ exists then update to new bt input path
+  sed -i 's%"/dev/input/"%'"\"${KEYCHRON_INPUT_PATH}\""'%g' "${KB_KMONAD_PATH}"
 
   sudo kmonad "${KB_KMONAD_PATH}"
 else 
